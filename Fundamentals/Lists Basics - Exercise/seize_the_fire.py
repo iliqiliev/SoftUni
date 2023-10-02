f6d257f82377706116f842f724a6edddcb49ae4c
Iliya Iliev <iliq123123@gmail.com>
2023-10-02 10:34:53 +0300

    Simplified code

--- Fundamentals/Lists Basics - Exercise/seize_the_fire.py
+++ Fundamentals/Lists Basics - Exercise/seize_the_fire.py
@@ -1,22 +1,28 @@
 input_fire = input().split("#")
 input_fire = [x.split(" = ") for x in input_fire]
 
-input_water = int(input())
+available_water = int(input())
 effort = 0.0
 fire_put_out = 0
 
+fires_info = {
+    "High": (81, 125),
+    "Medium": (51, 80),
+    "Low": (1, 50)
+}
+
 print("Cells:")
 for fire in input_fire:
-    fire[1] = int(fire[1])
-    if (fire[0] == "High" and 80 < fire[1] <= 125 or \
-       fire[0] == "Medium" and 50 < fire[1] <= 80 or \
-       fire[0] == "Low" and 0 < fire[1] <= 50) and \
-       input_water >= fire[1]:
+    fire_type, fire_level = fire[0], int(fire[1])
+    min_fire, max_fire = fires_info[fire_type]
+    if fire_type in fire and \
+       min_fire <= fire_level <= max_fire and \
+       available_water >= fire_level:
            
-        input_water -= fire[1]
-        effort += 0.25 * fire[1]
-        fire_put_out += fire[1]
-        print(f" - {fire[1]}")  
+        available_water -= fire_level
+        effort += 0.25 * fire_level
+        fire_put_out += fire_level
+        print(f" - {fire_level}")  
         
 print(f"Effort: {effort:.2f}\n"
       f"Total Fire: {fire_put_out}")
