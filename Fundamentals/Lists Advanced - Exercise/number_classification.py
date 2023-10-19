919141dfe2e38a107a7f452ff53fdec95cdd64f6
Iliya Iliev <iliq123123@gmail.com>
2023-10-19 17:33:52 +0300

    Simplified code

--- Fundamentals/Lists Advanced - Exercise/number_classification.py
+++ Fundamentals/Lists Advanced - Exercise/number_classification.py
@@ -1,9 +1,9 @@
-numbers = [int(x) for x in input().split(", ")]
+numbers = input().split(", ")
 
-positive = ", ".join([str(number) for number in numbers if number >= 0])
-negative = ", ".join([str(number) for number in numbers if number < 0])
-even = ", ".join([str(number) for number in numbers if not (number & 1)])
-odd = ", ".join([str(number) for number in numbers if (number & 1)])
+positive = ", ".join([number for number in numbers if int(number) >= 0])
+negative = ", ".join([number for number in numbers if int(number) < 0])
+even = ", ".join([number for number in numbers if not (int(number) & 1)])
+odd = ", ".join([number for number in numbers if (int(number) & 1)])
 
 print(f"Positive: {positive}\n"
       f"Negative: {negative}\n"
