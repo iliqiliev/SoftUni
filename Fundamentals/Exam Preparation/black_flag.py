91c9034dce014d42517dadf760c5048af632bd55
Iliya Iliev <iliq123123@gmail.com>
2023-10-21 15:03:00 +0300

    Formatting

--- Fundamentals/Exam Preparation/black_flag.py
+++ Fundamentals/Exam Preparation/black_flag.py
@@ -4,14 +4,18 @@ expected_plunder = float(input())
 plunder = 0
 
 for day in range(1, days + 1):
+    
     plunder += daily_plunder
+    
     if not (day % 3):
         plunder += daily_plunder * 0.5
+        
     if not (day % 5):
         plunder -= (plunder * 0.3)
 
 if plunder >= expected_plunder:
     print(f"Ahoy! {plunder:.2f} plunder gained.")
-else:  
-    plunder_percentage = plunder / expected_plunder * 100
+    
+else:
+    plunder_percentage = (plunder / expected_plunder) * 100
     print(f"Collected only {plunder_percentage:.2f}% of the plunder.")
