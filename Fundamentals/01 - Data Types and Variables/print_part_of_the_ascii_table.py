5a9564e3b458472ffd1677a9268f449d7cffb2d3
Iliya Iliev <iliq123123@gmail.com>
2023-11-02 22:43:10 +0200

    Renamed

--- Fundamentals/01 - Data Types and Variables/print_part_of_the_ascii_table.py
+++ Fundamentals/01 - Data Types and Variables/print_part_of_the_ascii_table.py
@@ -2,4 +2,4 @@ starting_char = int(input())
 ending_char = int(input())
 
 for ascii_index in range(starting_char, ending_char + 1):
-    print(chr(ascii_index), end=' ')
+    print(chr(ascii_index), end=" ")
