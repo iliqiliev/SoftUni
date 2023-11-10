f23bf776ba5c056bfdc23a41e32882ab68f3831e
Iliya Iliev <iliq123123@gmail.com>
2023-11-11 00:12:21 +0200

    Completed additional exercises

---
+++ Fundamentals/08 - Dictionaries/Dictionaries - More Exercise/judge.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+entries = defaultdict(lambda: defaultdict(int))
+
+while True:
+    command = input().split(" -> ")
+    if len(command) != 3:
+        break
+
+    name, contest, points = command
+    points = int(points)
+
+    entries[contest][name] = max(points, entries[contest].get(name, 0))
+
+
+for contest, participants in entries.items():
+    print(f"{contest}: {len(participants)} participants")
+
+    # sort the participants by the biggest score (-x[1])
+    # and then by name alphabetically (x[0])
+    ranked = enumerate(sorted(participants.items(), key=lambda x: (-x[1], x[0])), 1)
+    for index, (participant, score) in ranked:
+        print(f"{index}. {participant} <::> {score}")
+
+individual_standings = defaultdict(int)
+for participants in entries.values():
+    for participant, score in participants.items():
+        individual_standings[participant] += score
+
+print("Individual standings:")
+# The second part of the sorting key fixes the 66/100, was a pain to find because:
+# "If all sorting criteria fail, the order should be by order of input." (: (:
+ranked = enumerate(sorted(individual_standings.items(), key=lambda x: (-x[1], x)), 1)
+for index, (participant, score) in ranked:
+    print(f"{index}. {participant} -> {score}")
---
+++ Fundamentals/08 - Dictionaries/Dictionaries - More Exercise/ranking.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+
+contests_info = {}
+
+while True:
+    command = input().split(":")
+    if len(command) != 2:
+        break
+
+    contest_name, contest_password = command
+    contests_info[contest_name] = contest_password
+
+
+submissions = defaultdict(dict)
+
+while True:
+    command = input().split("=>")
+    if len(command) != 4:
+        break
+
+    contest_name, contest_password, username, points = command
+    points = int(points)
+
+    # if the provided password matches the one stored or None if password not stored
+    if contests_info.get(contest_name, None) == contest_password:
+        current = submissions[username]
+
+        current[contest_name] = max(points, current.get(contest_name, 0))
+
+# the best candidate is the one with the biggest sum of the inner dictionary values
+# best is a tuple consisting of the name and the dictionary with the scores
+best = max(submissions.items(), key=lambda score: sum(score[1].values()))
+best_name, best_score = best[0], sum(best[1].values())
+
+print(f"Best candidate is {best_name} with total {best_score} points.")
+print("Ranking:")
+for username, scores in sorted(submissions.items()):
+    print(username)
+
+    for contest, points in sorted(
+        scores.items(), key=lambda score: score[1], reverse=True
+    ):
+        print(f"#  {contest} -> {points}")
